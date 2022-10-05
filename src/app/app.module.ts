de6167f71d8d4f18d09df39816b6c1739e22f31e
Hieu Nguyen <hieu.nguyen@zaitenllc.com>
2022-10-05 21:32:04 +0700

    update product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,19 @@ import { HomeComponent } from './components/home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavComponent } from './components/nav/nav.component';
 import { CartComponent } from './components/cart/cart.component';
+import { ProductlistComponent } from './components/productlist/productlist.component';
+import { ProductComponent } from './components/product/product.component';
+import { SlideComponent } from './components/slide/slide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    CartComponent
+    CartComponent,
+    ProductlistComponent,
+    ProductComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule,
